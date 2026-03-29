4ff1e11468ba33026e4539309cdf23709bb40f93
Dyyynamic <philip.svensson.home@gmail.com>
2026-03-29 14:40:00 +0200

    Add system menu to Walker

---
+++ config/elephant/menus/system.lua
@@ -0,0 +1,32 @@
+Name = "system"
+NamePretty = "System"
+Icon = "system-shutdown"
+Terminal = false
+Cache = false
+
+Action = "%VALUE%"
+
+function GetEntries()
+    return {
+        {
+            Text = "Power Off",
+            Value = "systemctl poweroff",
+            Icon = "system-shutdown-symbolic",
+        },
+        {
+            Text = "Reboot",
+            Value = "systemctl reboot",
+            Icon = "system-reboot-symbolic",
+        },
+        {
+            Text = "Suspend",
+            Value = "systemctl suspend",
+            Icon = "system-suspend-symbolic",
+        },
+        {
+            Text = "Log Out",
+            Value = "hyprctl dispatch exit",
+            Icon = "system-log-out-symbolic",
+        }
+    }
+end
